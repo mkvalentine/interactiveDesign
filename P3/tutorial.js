8a75c10293a10921b4ec1e386fffda876aa9aab9
mkvalentine <megan.valentine@yale.edu>
2015-03-30 16:59:09 -0400

    update

---
+++ P3/tutorial.js
@@ -0,0 +1,41 @@
+/* javascript tutorial
+
+written in 1995
+google used it in 2005
+Node.js = server side javascript
+
+whole stack can be done in js
+Occulus uses js
+
+javascript: programming for HTML
+jQuery = library written in js, simplify client-side scripting of HTML
+
+<script type = 'text/javascript' src='js/script.js'></script>
+order in which you load things matters: load jQuery file first
+
+//comment
+
+use console.log(); to debug as you write in dev tools
+*/
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Tutorial</title>
+</head>
+<body>
+	 <script type = 'text/javascript'>
+	 var a = 5;
+	 var user = "cool dude";
+	 var bool = true;
+	 var length = user.length;
+	 console.log(a);
+	 console.log(user);
+	 console.log(bool);
+	 console.log(length);
+	 </script>
+</body>
+</html>
+
+
