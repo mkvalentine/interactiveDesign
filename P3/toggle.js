4b305fc49c8ddc9890293dc1f45446b5e6aa02a3
mkvalentine <megan.valentine@yale.edu>
2015-04-04 23:48:02 -0400

    update

---
+++ P3/toggle.js
@@ -0,0 +1,12 @@
+function toggleOverlay(){
+		var overlay = document.getElementById('overlay');
+		var specialBox = document.getElementById('specialBox');
+		overlay.style.opacity = .6;
+		if(overlay.style.display == "block"){
+			overlay.style.display = "none";
+			specialBox.style.display = "none";
+		} else {
+			overlay.style.display = "block";
+			specialBox.style.display = "block";
+		}
+	}
