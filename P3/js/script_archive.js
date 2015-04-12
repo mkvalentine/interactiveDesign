d99dc9ee4a7bfc697a62251c5584d7a00baf1440
mkvalentine <megan.valentine@yale.edu>
2015-04-11 22:44:20 -0400

    update

--- P3/js/script_archive.js
+++ P3/js/script_archive.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
 
 	$('#loonGif').hide();
+	$('#phoneCompass').hide();
+	$('#phoneCompassStatic').hide();
 
 	$('#title').toggle(function(){
 		$('#loonGif').show();
 		$('canvas').hide();
+		$('#phoneCompass').show();
+		$('#phoneCompassStatic').show();
 	},
 	function() {
         $('#loonGif').hide();
         $('canvas').show();
+        $('#phoneCompass').hide();
+		$('#phoneCompassStatic').hide();
 	});
 });
 
